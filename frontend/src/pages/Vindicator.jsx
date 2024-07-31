beec5e6c722fe56c7399cc91e80fa40ad34bf397
GiaVinhNguyen <vinh.g.nguyen19@gmail.com>
2024-07-31 01:08:04 -0400

    venomancer crud added

--- frontend/src/pages/Vindicator.jsx
+++ frontend/src/pages/Vindicator.jsx
@@ -5,7 +5,7 @@ export default function Vindicator() {
         "" : "The Frontend of Vinh's Vanguard. As the initiator of the operation, Vindicator plans and visualizes his victory through the mastery of HTML, CSS, JS, and React.",
         "crusader-sacred-smite": "Crusader Sacred Smite — Deploy stunning visual effects that captivate and mesmerize all who behold them, rendering adversaries awestruck.",
         "jouster-strike": "Jouster Strike — A knight's masterful technique to swiftly respond with intricate algorithms, executing commands with the speed and precision of a jousting blade.",
-        "react": "React — Empowers Vindicator to protect his allies with lightning-fast responses and unmatched adaptability. Vindicator dynamically adjusts tactics to counter any adversary",
+        "react": "React — Empowers Vindicator to protect his allies with lightning-fast responses and unmatched adaptability. Dynamically adjusts his tactics to counter any adversary",
         "holy-text-of-mighty-lord" : "Holy Text of Mighty Lord — Endowed with the Mighty Lord's sacred knowledge, this holy text blesses Vindicator and his allies the essential foundation for any digital crusade."
     };
 
